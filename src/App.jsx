4feea32ea16ab0cb8be3ab40f05b8bf40cfa0283
Andre Lima <andresdslima@gmail.com>
2022-05-05 00:19:28 +0200

    deploying app v1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import styles from "./App.module.css";
 import Board from "./components/Board";
 
 function App() {
   return (
-    <div className={styles.AppStyle}>
+    <>
       <h1>Memory Game</h1>
       <Board />
-    </div>
+    </>
   );
 }
 
